c2caed3d6896fa7d2885d81587ea767406e2efad
animesh-77 <animeshsrivasta77@gmail.com>
2024-02-24 21:55:55 +0000

    refactor: remove repetetive functions
    validated with previous outputs .csvs

--- task4.py
+++ task4.py
@@ -16,6 +16,25 @@ def score_Laplace(
     vocab_length: int,
     doc_length: int,
 ) -> float:
+    """
+    score_Laplace get query max Likelihood score with
+    Laplace smoothing
+
+    _
+
+    :param query: string of the query
+    :type query: str
+    :param pid: unique passage ID
+    :type pid: int
+    :param inverted_index: inverted index of whole vocabulary
+    :type inverted_index: dict
+    :param vocab_length: number of words in whole vocabulary
+    :type vocab_length: int
+    :param doc_length: Total number of words in the passage after stemming
+    :type doc_length: int
+    :return: Maximum likelihood estimate of the query from this passage
+    :rtype: float
+    """
 
     query_tokens = set(task1.work_one_line(query))
 
@@ -37,6 +56,25 @@ def top100_pids_score_Laplace(
     inverted_index: dict,
     vocab_length: int,
 ) -> pd.DataFrame:
+    """
+    top100_pids_score_Laplace Get at most the top 100 passages with highest
+    ML score for the specific query from the given passages and PIDs
+
+    While calculating the ML for query, Laplace smoothing is applied
+
+    :param qid: unique query ID
+    :type qid: int
+    :param query: suery string
+    :type query: str
+    :param unique_pids: List of all unique PIDS to iterate through
+    :type unique_pids: pd.Series
+    :param inverted_index: inverted index of whole vocabulary
+    :type inverted_index: dict
+    :param vocab_length: total number of words in the vocabulary
+    :type vocab_length: int
+    :return: dataframe with at most top 100 passages for this query
+    :rtype: pd.DataFrame
+    """
 
     qids = np.ones(unique_pids.shape[0], dtype=int) * qid
     scores = np.zeros(unique_pids.shape[0])
@@ -71,6 +109,12 @@ def top100_pids_score_Laplace(
 
 
 def Laplace_smoothing():
+    """
+    Laplace_smoothing Iterate over all qids, get at most top 100 pids
+    and save in  a .csv file
+
+    Laplace smoothing applied in query likelihood estimation
+    """
 
     inverted_index = task3.get_inverted_index()
     vocab_length = len(inverted_index.keys())
@@ -89,7 +133,8 @@ def Laplace_smoothing():
 
     qids, queries = task3.get_test_queries()
 
-    df = pd.DataFrame(columns=["qid", "pid", "score"])
+    df_laplace = pd.DataFrame(columns=["qid", "pid", "score"])
+    df_lidstone = pd.DataFrame(columns=["qid", "pid", "score"])
 
     for qid, query in tqdm(zip(qids, queries)):
         candidate_passages_df_qid = candidate_passages_df[
@@ -98,18 +143,35 @@ def Laplace_smoothing():
         qid_unique_pids = candidate_passages_df_qid["pid"]
         # work only with these pids and passages
 
-        qid_df = top100_pids_score_Laplace(
+        qid_df_laplace = top100_pids_score_Laplace(
             qid,
             query,
             qid_unique_pids,
             inverted_index,
             vocab_length,
         )
-        df = pd.concat([df, qid_df])
+        df_laplace = pd.concat([df_laplace, qid_df_laplace])
+
+        qid_df_lidstone = top100_pids_score_Lidstone(
+            qid,
+            query,
+            qid_unique_pids,
+            inverted_index,
+            vocab_length,
+        )
+        df_lidstone = pd.concat([df_lidstone, qid_df_lidstone])
+        # break
         # break
 
-    df.to_csv("laplace.csv", index=False, header=False)  # add header= False
+    df_laplace.to_csv(
+        "laplace.csv", index=False, header=False
+    )  # add header= False
     print("Done Saving top 100 Laplace scores of all queries as .csv file")
+
+    df_lidstone.to_csv(
+        "lidstone.csv", index=False, header=False
+    )  # add header= False
+    print("Done Saving top 100 Lidstone scores of all queries as .csv file")
 
 
 def score_Lidstone(
@@ -120,6 +182,27 @@ def score_Lidstone(
     doc_length: int,
     epsilon: float = 0.1,
 ) -> float:
+    """
+    score_Lidstone get Maximum likelihood estimate for the
+    given query and passage
+
+    Uses Lidstone smoothing while estimating ML for the query
+
+    :param query: query as a string
+    :type query: str
+    :param pid: unique passage ID
+    :type pid: int
+    :param inverted_index: inverted index for the whole vocabulary
+    :type inverted_index: dict
+    :param vocab_length: total number of words in the vocabulary
+    :type vocab_length: int
+    :param doc_length: total number of words in given passage/document
+    :type doc_length: int
+    :param epsilon: parameter for Lidstone correction, defaults to 0.1
+    :type epsilon: float, optional
+    :return: Maximum likelihood estimate for the given query
+    :rtype: float
+    """
 
     query_tokens = set(task1.work_one_line(query))
 
@@ -141,6 +224,25 @@ def top100_pids_score_Lidstone(
     inverted_index: dict,
     vocab_length: int,
 ) -> pd.DataFrame:
+    """
+    top100_pids_score_Lidstone For a given query
+    get at most the top 100 passages with highest query likelihdod
+
+    While estimating the ML score for a query Lidstone smoothing is applied
+
+    :param qid: unique query ID
+    :type qid: int
+    :param query: suery string
+    :type query: str
+    :param unique_pids: List of all unique PIDS to iterate through
+    :type unique_pids: pd.Series
+    :param inverted_index: inverted index of whole vocabulary
+    :type inverted_index: dict
+    :param vocab_length: total number of words in the vocabulary
+    :type vocab_length: int
+    :return: dataframe with at most top 100 passages for this query
+    :rtype: pd.DataFrame
+    """
 
     qids = np.ones(unique_pids.shape[0], dtype=int) * qid
     scores = np.zeros(unique_pids.shape[0])
@@ -174,48 +276,5 @@ def top100_pids_score_Lidstone(
     return top100_pid_score_df
 
 
-def Lidstone_smoothing():
-
-    inverted_index = task3.get_inverted_index()
-    vocab_length = len(inverted_index.keys())
-
-    candidate_passages_df = pd.read_table(
-        "candidate-passages-top1000.tsv",
-        delimiter="\t",
-        header=None,
-    )
-    candidate_passages_df.columns = [
-        "qid",
-        "pid",
-        "query",
-        "passage",
-    ]  # type:ignore
-
-    qids, queries = task3.get_test_queries()
-
-    df = pd.DataFrame(columns=["qid", "pid", "score"])
-
-    for qid, query in tqdm(zip(qids, queries)):
-        candidate_passages_df_qid = candidate_passages_df[
-            candidate_passages_df["qid"] == qid
-        ]
-        qid_unique_pids = candidate_passages_df_qid["pid"]
-        # work only with these pids and passages
-
-        qid_df = top100_pids_score_Lidstone(
-            qid,
-            query,
-            qid_unique_pids,
-            inverted_index,
-            vocab_length,
-        )
-        df = pd.concat([df, qid_df])
-        # break
-
-    df.to_csv("lidstone.csv", index=False, header=False)  # add header= False
-    print("Done Saving top 100 Lidstone scores of all queries as .csv file")
-
-
 if __name__ == "__main__":
     Laplace_smoothing()
-    Lidstone_smoothing()
